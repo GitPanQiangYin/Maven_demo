6ff8665bd7b4e9ad4b236eddda16ed0643090ab1
GitPanQiangYin <335238758@qq.com>
2020-07-17 13:28:14 +0800

    短信发送

---
+++ src/main/java/SendSms.java
@@ -0,0 +1,30 @@
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+
+public class SendSms {
+    public static void main(String[] args) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<accessKeyId>", "<accessSecret>");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        /*CommonRequest request = new CommonRequest();
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", "17329302587");
+        request.putQueryParameter("SignName", "·ä³²¿ìµÝ");
+        request.putQueryParameter("TemplateCode", "SMS_168822148");
+        request.putQueryParameter("TemplateParam", "{\"code\":\"linyiyi\"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }*/
+    }
+}
